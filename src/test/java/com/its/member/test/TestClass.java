dfd62e67fd2798c8fed56ea377414c0aa3d41c92
980219kch <104111005+980219kch@users.noreply.github.com>
2022-06-22 14:22:37 +0900

    회원가입 테스트 코드 수정

--- src/test/java/com/its/member/test/TestClass.java
+++ src/test/java/com/its/member/test/TestClass.java
@@ -2,6 +2,7 @@ package com.its.member.test;
 
 import com.its.member.dto.MemberDTO;
 import com.its.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,13 +16,20 @@ public class TestClass {
     @Autowired
     private MemberService memberService;
 
+    public MemberDTO newMember() {
+        MemberDTO memberDTO = new MemberDTO("테스트용이메일", "테스트용비밀번호", "테스트용이름", 99, "테스트용전화번호");
+        return memberDTO;
+    }
+
     @Test
-    @Transactional
-    @Rollback
+    @Transactional // 테스트클래스에서 쓰면 자동으로 롤백됨.
+    @Rollback(value = true)
+    @DisplayName("회원가입 테스트")
     public void saveTest() {
-        MemberDTO memberDTO = new MemberDTO("aa", "aa", "aa", 10, "aa");
-        Long saveId = memberService.save(memberDTO);
+//        MemberDTO memberDTO = new MemberDTO("aa", "aa", "aa", 10, "aa");
+//        Long saveId = memberService.save(memberDTO);
+        Long saveId = memberService.save(newMember());
         MemberDTO findDTO = memberService.findById(saveId);
-        assertThat(memberDTO.getMemberEmail()).isEqualTo(findDTO.getMemberEmail());
+        assertThat(newMember().getMemberEmail()).isEqualTo(findDTO.getMemberEmail());
     }
 }
