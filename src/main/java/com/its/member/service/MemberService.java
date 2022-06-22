964e80f37147035bd57b6ad6abb1a7b32abe2b25
980219kch <104111005+980219kch@users.noreply.github.com>
2022-06-22 11:04:36 +0900

    회원가입 기능완료, 테스트코드 작성 완료

--- src/main/java/com/its/member/service/MemberService.java
+++ src/main/java/com/its/member/service/MemberService.java
@@ -1,4 +1,32 @@
 package com.its.member.service;
 
+import com.its.member.dto.MemberDTO;
+import com.its.member.entity.MemberEntity;
+import com.its.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long save(MemberDTO memberDTO) {
+        MemberEntity memberEntity = MemberEntity.toEntity(memberDTO);
+        Long id = memberRepository.save(memberEntity).getId();
+        return id;
+
+    }
+
+    public MemberDTO findById(Long saveId) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(saveId);
+        if(optionalMemberEntity.isPresent()) {
+            return MemberDTO.toDTO(optionalMemberEntity.get());
+        } else {
+            return null;
+        }
+    }
 }
--- src/test/java/com/its/member/test/TestClass.java
+++ src/test/java/com/its/member/test/TestClass.java
@@ -1,4 +1,27 @@
 package com.its.member.test;
 
+import com.its.member.dto.MemberDTO;
+import com.its.member.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+@SpringBootTest
 public class TestClass {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Test
+    @Transactional
+    @Rollback
+    public void saveTest() {
+        MemberDTO memberDTO = new MemberDTO("aa", "aa", "aa", 10, "aa");
+        Long saveId = memberService.save(memberDTO);
+        MemberDTO findDTO = memberService.findById(saveId);
+        assertThat(memberDTO.getMemberEmail()).isEqualTo(findDTO.getMemberEmail());
+    }
 }
